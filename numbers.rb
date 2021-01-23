cba34a3bf4ec297bb56422c27b6be4efb8aac7e6
vkrish199 <31401485+vkrish199@users.noreply.github.com>
2021-01-23 11:20:21 +0530

    Update numbers.rb

--- numbers.rb
+++ numbers.rb
@@ -1,15 +1,14 @@
 class Numbers
 	CONST_VAR_NUM = 10
 	@@class_var_num = 5
-	def initialize(local_var_num)
-		@local_var_num_inst = local_var_num
-		@inst_var_num = 10
+	def initialize(local_var)
+		@inst_var_num = local_var
 	end
 
-	def add_nums
-		puts CONST_VAR_NUM + @@class_var_num + @inst_var_num + @local_var_num_inst
+	def add_nums(local)
+		puts CONST_VAR_NUM + @@class_var_num + @inst_var_num + local
 	end
 end
 
 numbers = Numbers.new(5)
-numbers.add_nums
+numbers.add_nums(10)
