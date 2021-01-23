1fd75325a28c478ef34d8f41434dce61623f2c38
vkrish199 <31401485+vkrish199@users.noreply.github.com>
2021-01-23 14:10:26 +0530

    Add files via upload

---
+++ students.rb
@@ -0,0 +1,7 @@
+student_data = [
+ {name: "varun", roll_no: "1000", marks: 90},
+ {name: "tejas", roll_no: "1001", marks: 95},
+ {name: "sandeep", roll_no: "1002", marks: 100}
+]
+
+puts student_data
