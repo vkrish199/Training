b99fc6b7529fb32f37057b6e40ec2af88ac0ec99
vkrish199 <31401485+vkrish199@users.noreply.github.com>
2021-01-24 10:36:12 +0530

    Add files via upload

---
+++ rectangle.rb
@@ -0,0 +1,25 @@
+class Rectangle
+
+	attr_accessor :area
+	attr_writer :length, :breadth
+
+	def initialize(length = nil, breadth = nil)
+		@length = length
+		@breadth = breadth
+		@area = nil
+	end
+
+	def compute_area
+		@area = @length*@breadth
+	end
+
+end
+
+rectangle = Rectangle.new
+
+rectangle.length = 10
+rectangle.breadth = 20
+
+rectangle.compute_area
+
+puts rectangle.area
