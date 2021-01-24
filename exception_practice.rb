030b6f7a420c7d74b6787caa9a9f7cd5d76f0fa0
vkrish199 <31401485+vkrish199@users.noreply.github.com>
2021-01-24 15:57:29 +0530

    Add files via upload

---
+++ exception_practice.rb
@@ -0,0 +1,22 @@
+class DivisionErrorException < ZeroDivisionError
+	attr_reader :reason
+	def initialize(reason)
+		@reason = reason
+		puts @reason
+	end
+end
+
+
+def test
+	n = 0
+	begin 
+		raise DivisionErrorException, "Divide by zero" if n == 0
+		r = 2/n
+		puts r
+	rescue DivisionErrorException => e
+	rescue Exception => e
+		puts e
+	end
+end
+
+puts test
