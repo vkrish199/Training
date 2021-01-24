378585e4822c6a4dbb5f0e2e1ecf4e5486b9e9fc
vkrish199 <31401485+vkrish199@users.noreply.github.com>
2021-01-24 14:28:48 +0530

    Add files via upload

---
+++ vehicle.rb
@@ -0,0 +1,46 @@
+class Vehicle
+
+	def initialize(no_of_tyres, seats)
+		@no_of_tyres = no_of_tyres
+		@seats = seats
+	end
+
+	protected
+
+	def protected_test
+		puts "Below is the Vehicle info"
+	end
+end
+
+class Car < Vehicle
+
+	def initialize(no_of_tyres, seats)
+		super
+	end
+
+	def moves
+		protected_test
+		puts "Car has #{@no_of_tyres} tyres and #{@seats} seats and moving"
+	end
+
+
+end
+
+class Bike < Vehicle
+
+	def initialize(no_of_tyres, seats)
+		super
+	end
+
+	def moves
+		protected_test
+		puts "Bike has #{@no_of_tyres} tyres and #{@seats} seats and moving"
+	end
+end
+
+
+car = Car.new(4, 5)
+car.moves
+
+bike = Bike.new(2,2)
+bike.moves
